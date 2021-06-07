36609265a0e306e3df3d34947dde3fc7515d1036
perezbarrera-julio <julioperezbarrera02@gmail.com>
2021-06-07 16:36:19 +0200

    LLamar a las vistas del Proyecto en eclipse

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,18 @@
 package main;
 
+import bbdd.conexion;
 import vista.Inicio;
+import vista.Login;
+import vista.Register;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		conexion.Conectar();
 		new Inicio();
+		new Login();
+		new Register();
 		
 	}
 
