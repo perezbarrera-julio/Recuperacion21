b86c4724c3d242d20fedeb7dc96a524bf085fc94
perezbarrera-julio <julioperezbarrera02@gmail.com>
2021-06-11 22:37:24 +0200

    Implementacion de acciones en la ventana de Login del Proyecto en eclipse

--- src/vista/Login.java
+++ src/vista/Login.java
@@ -5,8 +5,14 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controlador.Central;
+
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
 
@@ -17,12 +23,17 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		
+		btnEntrar.setBounds(43, 137, 89, 23);
+		contentPane.add(btnEntrar);
 		
 		JLabel lblNewLabel = new JLabel("New label");
 		lblNewLabel.setIcon(new ImageIcon("C:\\Proyecto\\ADAITS\\Programaci\u00F3n\\ProyectoEclipse\\Proyecto\\Imag\\Fondoatras.png"));
@@ -32,5 +43,11 @@ public class Login extends JFrame {
 		/* Hacer visible el boton  */
 		
 		setVisible(true);
+		
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new Central().abrirTabla();
+			}
+		});
 	}
 }
