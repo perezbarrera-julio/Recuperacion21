56056195f36596b3e0e3eff7a974e21a5425985d
perezbarrera-julio <julioperezbarrera02@gmail.com>
2021-06-13 19:50:48 +0200

    Cambios de acciones en la ventana de Login del Proyecto en eclipse

--- src/vista/Login.java
+++ src/vista/Login.java
@@ -1,9 +1,13 @@
 package vista;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 import controlador.Central;
@@ -11,12 +15,17 @@ import controlador.Central;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
+	private JPasswordField passwordField;
+	private JTextField textField;
 
 
 	/**
@@ -29,16 +38,51 @@ public class Login extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+	
 		
-		JButton btnEntrar = new JButton("Entrar");
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblUsuario.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsuario.setToolTipText("");
+		lblUsuario.setBackground(Color.WHITE);
+		lblUsuario.setForeground(new Color(255, 255, 102));
+		lblUsuario.setBounds(27, 36, 92, 20);
+		contentPane.add(lblUsuario);
 		
-		btnEntrar.setBounds(43, 137, 89, 23);
+		
+		JLabel lblPassW = new JLabel("Contraseña");
+		lblPassW.setToolTipText("");
+		lblPassW.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPassW.setForeground(new Color(255, 255, 102));
+		lblPassW.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		lblPassW.setBackground(Color.WHITE);
+		lblPassW.setBounds(27, 67, 103, 20);
+		contentPane.add(lblPassW);
+		
+		JButton btnEntrar = new JButton("Ingresar");
+		btnEntrar.setFont(new Font("Trebuchet MS", Font.PLAIN, 15));
+		
+		btnEntrar.setBounds(305, 180, 98, 38);
 		contentPane.add(btnEntrar);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Proyecto\\ADAITS\\Programaci\u00F3n\\ProyectoEclipse\\Proyecto\\Imag\\Fondoatras.png"));
-		lblNewLabel.setBounds(0, 0, 434, 261);
+		passwordField = new JPasswordField();
+		passwordField.setBounds(140, 67, 127, 20);
+		contentPane.add(passwordField);
+		
+		textField = new JTextField();
+		textField.setBounds(140, 39, 103, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Proyecto\\ADAITS\\Programaci\u00F3n\\Proyecto-MVC\\Imag\\image-removebg-preview (6).png"));
+		lblNewLabel.setBounds(50, 112, 171, 138);
 		contentPane.add(lblNewLabel);
+		
+		JLabel lblFondo = new JLabel("");
+		lblFondo.setIcon(new ImageIcon("C:\\Proyecto\\ADAITS\\Programaci\u00F3n\\ProyectoEclipse\\Proyecto\\Imag\\Fondoatras.png"));
+		lblFondo.setBounds(0, 0, 434, 261);
+		contentPane.add(lblFondo);
 		
 		/* Hacer visible el boton  */
 		
